d3a5cacf9591705a31217f857b08e8e4a8edcdd9
MariiaBaikova <baikova.maria.bm@gmail.com>
2023-01-02 19:01:05 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,95 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data, dataHotel } from './data';
 import './App.css';
 
 function App() {
+  const [city, setCity] = useState(0);
+  const {id, cityName, image, description} = data[city];
+  const [showMore, setShowMore] = useState(false);
+  const [hotels, setHotels] = useState(dataHotel);
+  
+
+
+  const previousCity = () => {
+    setCity((city => {
+      city --;
+      if (city < 0) {
+        return data.length-1;
+      }
+        return city;
+    }))
+  }
+  
+  const nextCity = () => {
+    setCity((city => {
+      city ++;
+      if (city > data.length - 1) {
+        city = 0;
+      }
+      return city;
+  } ))
+}
+const removeHotel = (id) => {
+  let newHotel = hotels.filter((hotel) =>
+    hotel.id !== id);
+    setHotels(newHotel)
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div>
+      <div className='container'>
+        <h1>TOP 5 cities in spain</h1>
+      </div>
+      <div className='container'>
+        <h2> {id} - {cityName}</h2>
+      </div>
+      <div className="container">
+        <img src={image} width="400px" alt="city"/>
+      </div>
+      <div className='container'>
+        <p>{showMore ? description : description.substring(0,200) + "..."}
+        <button className='btn' onClick = {() => setShowMore(!showMore)}>{showMore ? "Show less" : "Show more"}</button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+      <div className='container'>
+        <button className='previous' onClick={previousCity}>Previous</button>
+        <button className='next' onClick={nextCity}>Next</button>
+      </div>
+      <br></br>
+
+      <div className="container">
+        <h3>Top {hotels.length} hotels in Spain</h3>
+      </div>
+      { hotels.map((element => {
+        const {id, hotelName, description, image, source} = element;
+        return(
+          <div key={id}>
+          <div className="container">
+            <h2>{id} - {hotelName}</h2>
+          </div>
+          <div className="container">
+            <p>{description}</p>
+          </div>
+          <div className="container">
+            <img src={image} width="300px" alt="hotel"/>
+          </div>
+          <div className="container">
+            <p>{source}</p>
+          </div>
+          <div  className="container">
+            <button className='button' onClick={() => removeHotel(id)} >Remove</button>
+          </div>
+          </div>
+        )
+      }))}
+      <div className="container">
+        <button className='button' onClick={() => setHotels([])}>delete all</button>
+      </div>
+
+      </div>
+
+    
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,108 @@
+export const data = [
+    {   id: 1,
+        cityName: "Madrid",
+        description: "The Spanish capital of Madrid is a great place for a European vacation or a short weekend. Madrid is elegant and stylish, with ornate architecture and perfectly manicured gardens and parks. For fine art lovers, Madrid is a must. House of the Prado, filled with impressive works dating back to the 12th century, it's easy to see why art fanatics from all over the world flock to Madrid. The Buen Retiro Park is a popular destination for tourists and locals alike. With sumptuous fountains and extensive greenery, the park is a great place to escape the bustling and noisy center of Madrid.",
+        image: "https://images.unsplash.com/photo-1578305698944-874fa44d04c9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80", 
+        showMore: false
+    },
+    {   id: 2,
+        cityName: "Barcelona",
+        description: "Barcelona is the second largest city in Spain. Located in the region of Catalonia, Barcelona offers the perfect blend of culture and history in a modern, cosmopolitan metropolis. Vibrant, colorful and evocative, Barcelona goes beyond being photogenic and provides beautiful city vistas wherever you turn! Home of the famous Barcelona football club. City football fans head to the Camp Nou to watch the lads from Barça perform their magic on the pitch. The nightlife in Barcelona is fantastic and don't forget Gaudí's work. Bars and clubs line the streets, and music plays until early in the morning. If you want to attend cool parties, be sure to go to Barcelona!",
+        image: "https://images.unsplash.com/photo-1558642084-fd07fae5282e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80", 
+        showMore: false
+    },
+    {   id: 3,
+        cityName: "Seville",
+        description: "Seville is the third largest city in Spain and the capital of the Andalusia region. The city is considered the birthplace of classical flamenco dance and has become world famous for it. If you want to learn this traditional dance or watch the pros perform, head to the Triana area and watch the story unfold! Other key attractions in Seville include the Alcázar castle complex and the Gothic Cathedral of Seville. The Alcazar castle complex was built during the Moorish Almohad dynasty and has an interesting history. Another must-see in Seville is the 18th-century bullring called Plaza de Toros de la Maestranza, where for hundreds of years man fought the beast in the ultimate test of strength and courage.",
+        image: "https://images.unsplash.com/photo-1559386081-325882507af7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80", 
+        showMore: false
+    },
+    {   id: 4,
+        cityName: "Valencia",
+        description: "Valencia is located in the Mediterranean part of Spain. This vibrant city is home to an abundance of culture and art. The city has its own international airport from which domestic flights are made, as well as flights to other European cities. Valencia is considered the city of arts and sciences and is home to some impressive and futuristic buildings such as an oceanarium, a planetarium and an interactive museum. Since the city is located on the coast, it boasts beautiful beaches. During the summer months, these coastal stretches are filled with tourists, but it is worth remembering that Valencia can be visited all year round, as the temperatures here are quite comfortable.",
+        image: "https://images.unsplash.com/photo-1516028851695-10e2f35c5441?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80", 
+        showMore: false
+    },
+    {   id: 5,
+        cityName: "Malaga",
+        description: "Located on the Costa del Sol, Malaga is a popular destination for European tourists. There are a lot of resorts and hotels here, and the city also boasts good clean beaches. Many tourists often neglect the beautiful architecture and rich heritage of Malaga in favor of beach parties and water sports. Whatever your reason for traveling to Malaga, rest assured that you will be greeted with a smile and great hospitality. Like Valencia, Malaga has an international airport that flies regularly to many European destinations with a range of low cost airlines.",
+        image: "https://images.unsplash.com/photo-1572192070179-8235b832df64?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80", 
+        showMore: false
+    }
+
+]
+
+export const dataHotel = [
+    {   id: 1,
+        hotelName: "Seventy Barcelona",
+        description: "Seventy Barcelona has a shared lounge, terrace, a restaurant and bar in Barcelona. This 4-star hotel offers room service, a 24-hour front desk and free WiFi. Guests can use the spa and wellness centre with an indoor pool, fitness centre, and sauna, as well as an outdoor swimming pool.",
+        image: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/251247628.jpg?k=5bc66b226da90b941bbae9aa3e84ba5a817c9727bdde2fab2f4917ca39682c53&o=&hp=1",
+        source: "Booking.com"
+    },
+  
+    {   id: 2,
+        hotelName: "H10 Madison 4* Sup",
+        description: "All rooms at this Midtown-Manhattan hotel come with a marble bathroom and some rooms offer a view of the Cathedral. A minibar and a desk are also included in the rooms. The property boats a 7000-square-feet fitness centre and spa. The Villard is open for breakfast and weekend brunches. Lotte New York also has an onsite bakery, two lounges and in-room dining options.",
+        image: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/132970375.jpg?k=41487c80e820cca0e4b7116a14ec9256ae43377cd0346e4b2853a8f2877fc176&o=&hp=1",
+        source: "Booking.com"
+    },  
+  
+    {   id: 3,
+        hotelName: "Pestana Plaza Mayor Madrid",
+        description: "Less than 1 km from Mercado San Miguel and a 13-minute walk from Puerta de Toledo, the property offers a terrace and a bar. The accommodation provides a 24-hour front desk, spa and free WiFi is available.",
+        image: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/298172491.jpg?k=99cb4f2f65b9a24c08f7eaf1db7299d1e5e39e4816f2e090eefa8db6e7047695&o=&hp=1",
+        source: "Booking.com"
+    },
+
+    {   id: 4,
+        hotelName: "NH Collection Madrid Gran Vía",
+        description: "Ideally located in the heart of Madrid, NH Collection Madrid Gran Vía provides buffet breakfast and free WiFi throughout the property, only 400 m from Puerta del Sol. The stylish rooms and suites in the hotel are fitted with a coffee machine. Each room is comes with a private bathroom with a bath or shower",
+        image: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/287119062.jpg?k=949d90cc694ac5c225844295474ef365873ffab34e9ab97c0686d7a39c303f29&o=&hp=1",
+        source: "Booking.com"
+    },
+
+    {   id: 5,
+        hotelName: "Eurostars Guadalquivir",
+        description: "Featuring 4-star accommodation, Eurostars Guadalquivir is located in Seville, 1.3 km from Triana Bridge - Isabel II Bridge and 1.7 km from Alcazar Palace. With a fitness centre, the 4-star hotel has air-conditioned rooms with free WiFi, each with a private bathroom. Private parking is available on site.",
+        image: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/315736870.jpg?k=a6af43b4132276522a1ad6b34c852adb888f1e5822639b69af25f1f25082cbeb&o=&hp=1",
+        source: "Booking.com"
+    },
+
+    {   id: 6,
+        hotelName: "Soho Boutique Catedral",
+        description: "Conveniently located in the centre of Seville, Soho Boutique Catedral offers air-conditioned rooms, an outdoor swimming pool, free WiFi and a terrace. This 4-star hotel offers room service and a 24-hour front desk. Guests can enjoy Spanish dishes at the restaurant or have a cocktail at the bar.",
+        image: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/327062571.jpg?k=908c7e4e8220789237e658d2fe248dd3462287890824492b8280798e488b786c&o=&hp=1",
+        source: "Booking.com"
+    },
+
+    {   id: 7,
+        hotelName: "Only YOU Hotel Valencia",
+        description: "Featuring a fitness centre, a shared lounge as well as a bar, Only YOU Hotel Valencia is located in the centre of Valencia, 700 m from Norte Train Station. This 5-star hotel offers room service, a 24-hour front desk and free WiFi. Certain rooms at the property include a patio with a city view.",
+        image: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/319219419.jpg?k=77fadf48d1b9d5be379a27ee2b8d4258a7f40500404f8467a1c7d660f5f2f752&o=&hp=1",
+        source: "Booking.com"
+    },
+
+    {   id: 8,
+        hotelName: "Palacio Vallier 5*",
+        description: "Situated in Valencia, 300 m from Church of Saint Nicolás, Palacio Vallier 5* features accommodation with a terrace, private parking, a restaurant and a bar. This 5-star hotel offers a business centre and a concierge service. The accommodation offers a 24-hour front desk, airport transfers, room service and free WiFi throughout the property.",
+        image: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/229276276.jpg?k=364cdd36089c5096099f3f79c707af3f704f719e0b39a9d25bfce1cf3e0696b8&o=&hp=1",
+        source: "Booking.com"
+    },
+
+    {   id: 9,
+        hotelName: "Only YOU Hotel Málaga",
+        description: "Situated in Málaga and with La Malagueta Beach reachable within 1.5 km, Only YOU Hotel Málaga features concierge services, allergy-free rooms, an outdoor swimming pool, free WiFi and a bar. This 5-star hotel offers room service and a 24-hour front desk. The property is 1.8 km from La Caleta Beach, and within 600 m of the city centre.",
+        image: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/328622834.jpg?k=8d21435a884e8c356337e6e246f77e5b81a7d4c2045b4396c19aca59003afd7f&o=&hp=1",
+        source: "Booking.com"
+    },
+
+
+    {   id: 10,
+        hotelName: "Soho Boutique Equitativa",
+        description: "Soho Boutique Equitativa features an outdoor swimming pool, terrace, a restaurant and bar in Málaga. With free WiFi, this 4-star hotel offers room service and a 24-hour front desk. The property is 600 m from the city centre and 1.5 km from La Malagueta Beach.",
+        image: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/307805164.jpg?k=a1882231891a12451cf89c920a300d1886b8119eb1cf226f4533a9d0abc4be3c&o=&hp=1",
+        source: "Booking.com"
+    }
+  
+  ]
+
